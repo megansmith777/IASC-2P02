421a6c62fa53e62c706e7462f208c28db28c135c
Megan Smith <megansmith@Megans-Air-168.lan>
2025-01-26 23:38:11 -0500

    Add Prototype 1

--- Template/script.js
+++ Template/script.js
@@ -1,18 +1,14 @@
 import * as THREE from "three"
-
-/**********
- **SCENE**
- **********/
-//Canvas
+/***********
+ ** SCENE ** 
+ ***********/
+// Canvas
 const canvas = document.querySelector('.webgl')
-
-//Scene
+// Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('pink')
-
-
-//Camera 
-const camera = new THREE.perspectiveCamera(
+scene.background = new THREE.Color('gray')
+// Camera
+const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
@@ -20,40 +16,32 @@ const camera = new THREE.perspectiveCamera(
 )
 scene.add(camera)
 camera.position.set(0, 0, 5)
-
-//Renderer
+// Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.setSize(window.innerWidth, window.innerHeight,)
-
-/**********
- **MESHES**
- **********/
+renderer.setSize(window.innerWidth, window.innerHeight)
+/***********
+** MESHES **
+************/
 // testSphere
 const sphereGeometry = new THREE.SphereGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-
 scene.add(testSphere)
-testSphere.position,set(0, 0, -5)
-
 /*******************
- **ANIMATION LOOP**
- *******************/
+** ANIMATION LOOP **
+********************/
 const clock = new THREE.Clock()
-
 const animation = () =>
 {
-   
-    //Return elapsedTime 
+    // Return elapsedTime
     const elapsedTime = clock.getElapsedTime()
-    console.log(elpasedTime)
-    //Renderer
+    
+    // Renderer
     renderer.render(scene, camera)
-    //Request next frame 
+    // Request next frame
     window.requestAnimationFrame(animation)
 }
-
 animation()
